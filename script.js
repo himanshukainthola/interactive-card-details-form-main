81aafaf2bfbdcf5b0aeb3dde0345830cd5c7298a
Himanshu Kainthola <hkainthola6@gmail.com>
2023-02-15 17:33:53 +0530

    will add more functionality later

--- script.js
+++ script.js
@@ -1,49 +1,38 @@
-const nameDisplay = document.querySelector('.name-on-card');
-const nameInput = document.querySelector('#cardholder-name');
+const id = (id) => document.querySelector(id);
 
-const numberDisplay = document.querySelector('.number-on-card');
-const numberInput = document.querySelector('#card-number');
+const clas = (clas) => document.querySelector(clas);
 
-const expDateDisplay = document.querySelector('.date-on-card');
-const expMonthDisplay = document.querySelector('.month');
-const expMonthInput = document.querySelector('#month');
-
-const expYearDisplay = document.querySelector('.year');
-const expYearInput = document.querySelector('#year');
-
-const cvcDisplay = document.querySelector('.cvc-on-card');
-const cvcInput = document.querySelector('#cvc');
-
-const confirmBtn = document.querySelector('.form-btn');
-
-const form = document.querySelector('.form');
-const messageDisplay = document.querySelector('.form-message');
-
-
-
+const nameInput = id('#cardholder-name'),
+    numberInput = id('#card-number'),
+    expMonthInput = id('#month'),
+    expYearInput = id('#year'),
+    cvcInput = id('#cvc'),
+    nameError = clas('.name-error'),
+    numberError = clas('.number-error'),
+    cvcError = id('#cvc-error'),
+    nameDisplay = clas('.name-on-card'),
+    numberDisplay = clas('.number-on-car'),
+    expDateDisplay = clas('.date-on-card'),
+    expMonthDisplay = clas('.month'),
+    expYearDisplay = clas('.year'),
+    cvcDisplay = clas('.cvc-on-card'),
+    messageDisplay = clas('.form-message'),
+    confirmBtn = clas('.form-btn'),
+form = clas('.form');
 
 
 
-nameInput.addEventListener('input', () =>{
-    nameDisplay.innerText = nameInput.value
-})
-numberInput.addEventListener('input', () =>{
-    numberDisplay.innerText = numberInput.value
-})
-expMonthInput.addEventListener('input', () =>{
-    expMonthDisplay.innerText = expMonthInput.value
-})
-expYearInput.addEventListener('input', () =>{
-    expYearDisplay.innerText = expYearInput.value
-})
-cvcInput.addEventListener('input', () =>{
-    cvcDisplay.innerText = cvcInput.value
-})
 
 
-confirmBtn.addEventListener('click', function(event){
-    form.classList.add('hidden');
-    messageDisplay.classList.remove('hidden');
 
-    event.preventDefault();
-})
+
+
+
+
+
+nameInput.addEventListener('input', () => nameDisplay.innerText = nameInput.value)
+numberInput.addEventListener('input', () => numberInput.innerText = numberInput.value)
+expMonthInput.addEventListener('input', () => expMonthDisplay.innerText = expMonthInput.value)
+expYearInput.addEventListener('input', () => expYearDisplay.innerText = expYearInput.value)
+cvcInput.addEventListener('input', () => cvcDisplay.innerText = cvcInput.value)
+form.addEventListener('submit', validate);
